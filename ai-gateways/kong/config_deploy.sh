a66a1d4c3132c70c8fb29cbffa00f2d4b347491f
Mohammad Bahmani <bahmani.m.n@gmail.com>
2026-05-16 20:37:31 +0200

    config with deck for kong

--- ai-gateways/kong/config_deploy.sh
+++ ai-gateways/kong/config_deploy.sh
@@ -7,6 +7,8 @@ JOB_FILE="config_job.yaml"
 FILE="test_service_route.yaml"
 JOB_NAME=$(grep "name:" "$JOB_FILE" | head -1 | awk '{print $2}')
 
+DEBUG_POD="kong-debug"
+
 echo "======================================"
 echo " 1. Create/Update ConfigMap"
 echo "======================================"
@@ -21,20 +23,20 @@ echo "======================================"
 echo "Create persistent debug pod (sleep 10 min)"
 echo "======================================"
 
-# oc create pod $DEBUG_POD --image=curlimages/curl:latest -n "$NAMESPACE" --restart=Never -- sleep 600 || true
-oc apply -f - <<EOF
-apiVersion: v1
-kind: Pod
-metadata:
-  name: ${DEBUG_POD}
-  namespace: ${NAMESPACE}
-spec:
-  containers:
-    - name: curl
-      image: curlimages/curl:8.20.0
-      command: ["sh", "-c", "sleep 600"]
-  restartPolicy: Never
-EOF
+oc create pod $DEBUG_POD --image=curlimages/curl:latest -n "$NAMESPACE" --restart=Never -- sleep 600 || true
+# oc apply -f - <<EOF
+# apiVersion: v1
+# kind: Pod
+# metadata:
+#   name: ${DEBUG_POD}
+#   namespace: ${NAMESPACE}
+# spec:
+#   containers:
+#     - name: curl
+#       image: curlimages/curl:8.20.0
+#       command: ["sh", "-c", "sleep 600"]
+#   restartPolicy: Never
+# EOF
 
 echo "Waiting for debug pod..."
 oc wait --for=condition=Ready pod/$DEBUG_POD -n "$NAMESPACE" --timeout=120s
